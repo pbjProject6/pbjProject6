df0ebc88fd36bd6a1a5b2321bdb5bdfa1b3627c2
Phil <bourassa.phil@gmail.com>
2018-09-06 19:21:13 -0400

    Fixed ROuting issue. Now all the  buttons we have so far will point to the right component(pages).

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+    Link
+} from 'react-router-dom';
+
+class Home extends Component {
+    render() {
+        return (
+            <section className="homeSection">
+                <header className="App-header">
+                    {/* <img src={logo} className="App-logo" alt="logo" /> */}
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p>James</p>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.</p>
+
+                <Link to="/TeamSelect"><button className="createNewTeamButton button">Create New Team</button></Link>
+
+                <Link to="/TeamSelect"><button className="loadTeamButton button">Load Exsting Team</button></Link>
+
+            </section>
+        )
+    }
+}
+
+export default Home;
