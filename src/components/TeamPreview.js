eb58ad90a53a5f244e01cb76bf2774e2bb6fa92a
James MacDonald <jamesdavidmacdonald@gmail.com>
2018-09-09 00:48:55 -0400

    Can Compete over stats, rematch, get new opponent, and go back to team

--- src/components/TeamPreview.js
+++ src/components/TeamPreview.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Link, Redirect } from "react-router-dom";
 import firebase from "firebase";
+import swal from 'sweetalert';
 
 
 
 let enemyTeams = [];
+let statNames = ['com', 'dur', 'int', 'pow', 'spd', 'str' ];
 const dbRef = firebase.database().ref('/teams');
 
 
@@ -12,11 +14,12 @@ class TeamPreview extends Component {
     constructor(){
         super();
         this.state = {
-            fightingEnemyTeam: null
+            fightingEnemyTeam: null,
+            redirect: false
         }
     }
     componentDidMount(){
-
+        enemyTeams = [];
         let teamNumRef;
             //get total number of teams in database
 
@@ -36,8 +39,11 @@ class TeamPreview extends Component {
                 // if the randomly generated team is the players team
                 if(enemyTeams[enemyNum].teamName === this.props.playerTeam.teamName){
                     //remove it and get a new one
-                    let playersTeam = enemyTeams.splice([enemyNum, 1]);
+                    console.log(enemyTeams.length);
+                    
+                    let playersTeam = enemyTeams.splice(enemyNum, 1);
                     console.log('removes players team');
+                    console.log(enemyTeams);
                     
                     enemyNum = Math.floor(Math.random() * enemyTeams.length);
                 }
@@ -51,10 +57,86 @@ class TeamPreview extends Component {
     }
 
     startBattle = () => {
-        
+        console.log("starting battle");
+        //generate random number between 0 and 5 to determine stat compared
+        let competeStat = Math.floor(Math.random() * statNames.length);
+        let playerWins = 0;
+        let enemyWins = 0;
+
+        // compare each character to the character in the respective slot on the other team
+        for( let i = 0; i <= 4; i++){
+            //api stores stats as strings, must convert them to numbers to compare them
+            let playerStat = parseInt(this.props.playerTeam.teamMember[i].stats[statNames[competeStat]], 10);
+            let enemyStat = parseInt(this.state.fightingEnemyTeam.teamMember[i].stats[statNames[competeStat]], 10);
+
+            console.log(playerStat, enemyStat);
+
+            // calculate character wins by the larger stat
+            if(playerStat >= enemyStat){
+                console.log(`player wins match ${i}`);
+                playerWins++;
+            }
+            else{
+                console.log(`enemy wins match ${i}`);
+                enemyWins++;
+            }
+        }
+        // display match results to player and update ratios
+        if(playerWins > enemyWins){
+            swal(`Competed in ${statNames[competeStat]}`, `You win with ${playerWins} wins to ${enemyWins}`).then(() => {
+                this.postBattleChoices();
+            });
+        }
+        else{
+            swal(`Competed in ${statNames[competeStat]}`, `The opponent won with ${enemyWins} wins to ${playerWins}`).then(() =>{
+                this.postBattleChoices();
+            });
+        }
+    }
+    postBattleChoices = () =>{
+
+        swal({
+            text: 'What would you like to do next?',
+            buttons: {
+                rematch: {
+                    text: 'Fight again?',
+                    value: 'rematch'
+                },
+                newOpponent: {
+                    text: 'Get a new Opponent?',
+                    value: 'newOpponent'
+                },
+                adjustTeam: {
+                    text: 'Adjust Team?',
+                    value: 'adjustTeam'
+                }
+
+
+            }
+        }).then((res) => {
+            console.log(res);
+            if(res === 'rematch'){
+                //fight same opponent again
+                this.startBattle();
+            }
+            else if(res === 'newOpponent'){
+                //get a new random opponent
+                this.componentDidMount();
+            }
+            else if(res === 'adjustTeam'){
+                // go back to team select
+                this.redirectToPage();
+            }
+        })
+    }
+    redirectToPage = () => {
+        //change state so the if statemnt in render method will redirect back to team page
+        this.setState({ redirect: true });
     }
     render(){
-        
+        if (this.state.redirect) {
+            return <Redirect push to="/teamselect" />;
+        }
         return(
             <div className="teamPreview">
                 <section className="playerTeam">
@@ -82,7 +164,7 @@ class TeamPreview extends Component {
                 </section>
                 }
                 <section className="teamButtons">
-                    <Link to="/teampreview">
+                    <Link to="/teamSelect">
                         <button>Back To Team Roster</button>
                     </Link>
                     <button onClick={this.startBattle}>Start Battle</button>
